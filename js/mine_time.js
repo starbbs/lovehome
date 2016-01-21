35ee13f1bf4af07cfc367e2b51b04bd9538bf8b4
rensomnus <renlixian@GR-YANFA-0004.new4g.local>
2016-01-21 17:00:23 +0800

    详情页面

--- js/mine_time.js
+++ js/mine_time.js
@@ -12,10 +12,11 @@ $(".btn").on("tap",function(){
 $(function(){ 
 		show_time();
 	});
-	function show_time(){ 
-	var time_start = new Date().getTime(); //设定当前时间
-	var time_end =  new Date("2016/1/22 00:00:00").getTime(); //设定目标时间
-	var time_distance = time_end - time_start; 
+	function show_time(){
+	var time_start = new Date("2016/1/21 11:50:00").getTime();	
+	var time_now = new Date().getTime(); //设定当前时间
+	var time_end =  new Date(time_start+259200000).getTime(); //设定目标时间
+	var time_distance = time_end - time_now; 
 	var int_day = Math.floor(time_distance/86400000) 
 	time_distance -= int_day * 86400000; 
 	var int_hour = Math.floor(time_distance/3600000) 
